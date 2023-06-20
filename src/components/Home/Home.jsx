f3ed6e7c9f476f47491e4079e99d10b17e8b1cfb
Akshay2004 <51174148+PopeRaj@users.noreply.github.com>
2023-06-20 00:22:20 -0400

    Current code

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import './Home.css';
+
+export default function Home(props) {
+    return (
+    <div id = "home_wrapper">
+        <div id = "home">
+            <h2> Hey, I'm</h2>
+            <h1> Akshay Aravind</h1>
+        </div>
+    </div>
+    )
+
+}
